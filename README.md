a31f2afc11ad6e3d2221c7fc17029ad1e8246da9
Gonzalo <santomegonzalo@gmail.com>
2013-12-11 14:10:36 -0300

    First commit
    First version of backgrid.fixedheader

--- README.md
+++ README.md
@@ -1,4 +1,58 @@
-backgrid.fixedheader
+Introduction
 ====================
 
-Backgrid don't support fixed header. So this is a plugin to make a fixed header on backgrid. Only works with backgrid.
+Backgrid don't support fixed header and sometime this feature is really necessary. 
+
+This plugin will make the fix header on your tables with backgrid. 
+
+It's ready to use on production but if someone want to help it's not a problem… maybe it will be nice to make everything inside backgrid.
+
+How to use
+==========
+
+Changes on Backgrid
+-------------------
+1- Add two variables in Grid (removeHeader, removeBody)
+    
+    var Grid = Backgrid.Grid = Backbone.View.extend({  
+    	removeHeader: false,  
+    	removeBody: false,  
+
+2- Inside initialize add this code (Inside Grid)
+
+    if( options.removeHeader ) {
+    	this.removeHeader = options.removeHeader;
+    }
+    
+    if( options.removeBody ) {
+    	this.removeBody = options.removeBody;
+    }
+    
+3- Add this functions inside Grid
+
+     removeBodyAction: function() {
+       if( this.removeBody ) {
+          this.$el.addClass('remove_body');
+       }
+     },
+     fixedHeaderAction: function() {
+       if( this.removeHeader ) {
+          this.$el.addClass('remove_header');
+       }
+     },
+
+Using the plugin
+--
+     $.backgridFixedHeader({
+       grid: this.grid, //this is the grid variable
+       container: this.$el.find('.backgrid-container') //this is the container where you want to put backgrid
+     });
+
+instead of 
+
+     this.$el.find('.backgrid-container').append(this.grid.render().$el);
+
+
+Making some changes on CSS
+--
+The container need to have  <b>overflow: hidden;</b> and <b>position: relative;</b>. Don't forget to add the css file too.
---
+++ backgrid.fixedheader.js
@@ -0,0 +1,107 @@
+/*!
+ * backgrid.fixedheader. The jQuery plugin to make backgrid with Fixed header
+ *
+ * Copyright (c) 2013 Gonzalo Santome
+ * http://www.boxdm.com
+ *
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * http://docs.jquery.com/Plugins/Authoring
+ * jQuery authoring guidelines
+ *
+ * Launch  : October 2013
+ * Version : 0.1.0
+ * Released: Dec 11th, 2013
+ *
+ */
+
+(function ( $ ) {
+	"use strict";
+	
+	$.backgridFixedHeader = function( options ) {
+		var VERSION = "0.1.0";
+		
+		var settings = {};
+		
+		var defaults = {
+			gridHeader: undefined	
+		};
+		
+		/*
+		 * Public methods
+		 */
+		var methods = {
+			init: function(options) {
+				//merge the options
+				settings = $.extend({}, defaults, options);
+				
+				//setting the scroll event
+				settings.container.bind('scroll', helpers._scrollingContainer);
+				//setting the window size event
+				$(window).bind('resize', helpers._resize);
+				
+				//generate the header backgrid
+				helpers._generateHeader.apply(this);
+				
+				//render everything
+				helpers._render.apply(this);
+			}			
+		};
+		
+		/*
+		 * Private methods
+		 */
+		var helpers = {
+			_render: function() {
+				helpers._generateHeader.apply(this);
+				
+				//add the containers for the grids ==>
+				settings.container.append('<div class="backgrid-fixed-header"></div>');
+				settings.container.append('<div class="backgrid-fixed-content"></div>');
+				// <==
+				
+				settings.container.find('.backgrid-fixed-content').bind('scroll', helpers._scrollingContainer);
+				
+				// render both grids ==>
+				settings.grid.render();
+				settings.gridHeader.render();
+				// <==
+				// append elements to containers ==>
+				settings.container.find('.backgrid-fixed-header').append(settings.gridHeader.$el);
+				settings.container.find('.backgrid-fixed-content').append(settings.grid.$el);
+				// <==
+				//execute new backgrid actions ==>
+				settings.gridHeader.removeBodyAction();
+				settings.grid.fixedHeaderAction();
+				// <==
+			},
+			_generateHeader: function() {
+				settings.gridHeader = new Backgrid.Grid({
+					columns: settings.grid.columns,
+					collection: settings.grid.collection,
+					removeBody: true
+				});
+				
+				//listen when the collection get more items
+				settings.grid.collection.bind("add", function(){
+					helpers._resize();
+				});
+			},
+			_scrollingContainer: function(evt) {
+				var $element = $(evt.target);
+				var top = $element.scrollTop();
+				var left = $element.scrollLeft();
+				
+				settings.container.find('.backgrid-fixed-header').css('margin-left', -Math.abs(left));
+			},
+			_resize: function(evt) {
+				var width = settings.container.find('.backgrid-fixed-content').find('.backgrid').width();
+				settings.container.find('.backgrid-fixed-header').width(width);
+			}
+		}
+		
+		methods.init.apply(this, arguments);
+	};
+	
+}( jQuery ));
